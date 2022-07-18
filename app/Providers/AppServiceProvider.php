211461d05b2843a878d37d143162098df3a1163b
Ryo Kawasaki <ryokawasaki@RyonoMBP.lan>
2022-07-19 03:08:22 +0900

    fix global variable.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $is_production = env('APP_ENV') === 'production' ? true : false;
+        view()->share('is_production', $is_production);
+
         if (request()->isSecure()) {
             \URL::forceScheme('https');
         }
